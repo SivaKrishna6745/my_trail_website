a3852ead78aeabe7d0de900bd92139051d43eff9
SivaKrishna6745 <krishnasiva6745@gmail.com>
2022-08-28 12:20:41 +0530

    contact page revamp

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,50 +3,87 @@ import { Button } from "react-bootstrap";
 import "./Contact.scss";
 
 function Contact() {
-    const contactName = useRef();
+    const firstName = useRef();
+    const lastName = useRef();
     const contactNumber = useRef();
     const contactMessage = useRef();
+    const contactEmail = useRef();
     function handleClick(e) {
         e.preventDefault();
         const subject = contactMessage.current.value;
         const body =
             "I am: " +
-            contactName.current.value +
+            firstName.current.value +
+            " " +
+            lastName.current.value +
             ";  My mobile number: " +
-            contactNumber.current.value;
+            contactNumber.current.value +
+            "; My mail id: " +
+            contactEmail.current.value;
         window.location = `mailto:krishnasiva6745@gmail.com?subject=${subject}&body=${body}`;
     }
     return (
-        <form
-            method="POST"
-            encType="multipart/form-data"
-            onSubmit={handleClick}
-        >
-            <div className="form-control">
-                <label htmlFor="name">Name:</label>
-                <input type="text" ref={contactName} name="contact-name" />
+        <div className="contact">
+            <div>
+                <img src="images/contact.png" alt="contact" />
             </div>
-            <div className="form-control">
-                <label htmlFor="phnno">Mobile Number:</label>
-                <input
-                    type="number"
-                    ref={contactNumber}
-                    name="contact-number"
-                />
-            </div>
-            <div className="form-control">
-                <label htmlFor="message">Message:</label>
-                <textarea
-                    cols="100"
-                    rows="5"
-                    ref={contactMessage}
-                    name="contact-message"
-                ></textarea>
-            </div>
-            <Button type="submit" variant="outline-primary">
-                Submit
-            </Button>
-        </form>
+            <form
+                className="form"
+                method="POST"
+                encType="multipart/form-data"
+                onSubmit={handleClick}
+            >
+                <div className="group">
+                    <input
+                        type="text"
+                        ref={firstName}
+                        placeholder="first name"
+                        className="control"
+                        name="first-name"
+                    />
+                </div>
+                <div className="group">
+                    <input
+                        type="text"
+                        ref={lastName}
+                        placeholder="last name"
+                        className="control"
+                        name="last-name"
+                    />
+                </div>
+                <div className="group">
+                    <input
+                        type="number"
+                        ref={contactNumber}
+                        placeholder="Enter your mobile number"
+                        className="control"
+                        name="contact-number"
+                    />
+                </div>
+                <div className="group">
+                    <input
+                        type="email"
+                        ref={contactEmail}
+                        placeholder="Enter your mail"
+                        className="control"
+                        name="contact-mail"
+                    />
+                </div>
+                <div className="group">
+                    <textarea
+                        cols="63"
+                        rows="4"
+                        ref={contactMessage}
+                        placeholder="Enter your message"
+                        className="control"
+                        name="contact-message"
+                    ></textarea>
+                </div>
+                <Button type="submit" variant="outline-primary">
+                    Submit
+                </Button>
+            </form>
+        </div>
     );
 }
 
